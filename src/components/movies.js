1a8b6453967a50d9ccdca4505bfd904e6bfdc318
docilefiasco <epshita.hs@gmail.com>
2022-06-08 16:22:19 +0530

    movies section made responsive

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,16 +1,23 @@
 import '../App.css'
 import Card from "./card";
-import data from "./data,";
+import moviesData from "./moviesdata";
+import ScrollArrow from './scrollbutton';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from './footer'
+import NavbarNew from './navbar';
 import Sidebar from './sidebar'
 
 export default function Movies() {
-  const cards = data.map((item) => {
+  const cards = moviesData.map((item) => {
     return <Card key={item.id} item={item} />;
   });        
   return (
     <div>
-      <Sidebar />
-       {cards}
+      <NavbarNew />
+      <div className="movies">{cards}</div>
+      <Footer />
+      <ScrollArrow />
     </div>
   );
 }
